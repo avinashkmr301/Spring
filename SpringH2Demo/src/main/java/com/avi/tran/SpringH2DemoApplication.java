86bbb631fa13a643755c226b7817a1f996bfe75b
Avinash <Avinash@Avinash-NXM8ESI00234.bbrouter>
2023-05-19 00:20:06 +0530

    Springcore

---
+++ SpringH2Demo/src/main/java/com/avi/tran/SpringH2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.avi.tran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2DemoApplication.class, args);
+	}
+
+}
---
+++ SpringH2Demo/src/test/java/com/avi/tran/SpringH2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avi.tran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
